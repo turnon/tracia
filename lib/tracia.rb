ad6d178afec3fd090ea15486d455bb1688d28d4c
ken <block24block@gmail.com>
2022-05-05 23:09:33 +0800

    init

---
+++ lib/tracia.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tracia/version"
+
+module Tracia
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "tracia"
+
+require "minitest/autorun"
