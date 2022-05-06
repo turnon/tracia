31c79c22d4762d8f765a77a6e13681d038fb9326
ken <block24block@gmail.com>
2022-05-06 17:49:53 +0800

    init

--- lib/tracia.rb
+++ lib/tracia.rb
@@ -4,5 +4,50 @@ require_relative "tracia/version"
 
 module Tracia
   class Error < StandardError; end
+
+  attr_accessor :level, :error
+
   # Your code goes here...
+  class << self
+    def start
+      trc = (Thread.current[:_tracia_] ||= new)
+      trc.level += 1
+      yield
+    rescue StandardError => e
+      trc.error = e
+      raise e
+    ensure
+      trc.level -= 1
+      Thread.current[:_tracia_] = nil if trc.error || trc.level == 0
+      trc.log if trc.level == 0 && trc.error.nil?
+    end
+
+    def add(attrs)
+      trc = Thread.current[:_tracia_]
+      trc.add(caller, attrs) if trc
+    end
+  end
+
+  class Frame
+    attr_reader :children
+
+    def initialize
+      @name = nil
+      @children = []
+      @data = []
+    end
+  end
+
+  def initialize
+    @stacks = []
+    @level = 0
+  end
+
+  def add(stack, data)
+    @stacks << [stack, data]
+  end
+
+  def log
+    puts @stacks
+  end
 end
