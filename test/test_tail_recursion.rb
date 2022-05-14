bd714c19d130497091719a8a68314ebe33ec6088
ken <block24block@gmail.com>
2022-05-14 15:43:46 +0800

    fix expected result in recursion test

--- test/test_tail_recursion.rb
+++ test/test_tail_recursion.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class TestTailRecursion < Minitest::Test
   class SomeRecursion
-    def run(n: 5)
+    def run(n: 3)
       walk(n)
       swim
       1 / n
@@ -29,13 +29,52 @@ class TestTailRecursion < Minitest::Test
     end
   end
 
+  EXPECTED = <<EOS
+TestTailRecursion#block in test_tail_recursion #{__dir__}/test_tail_recursion.rb:71
+├─TestTailRecursion::SomeRecursion#fly #{__dir__}/test_tail_recursion.rb:14
+│ └─{:msg=>\"parallel call\"}
+├─TestTailRecursion::SomeRecursion#run #{__dir__}/test_tail_recursion.rb:7
+│ ├─TestTailRecursion::SomeRecursion#walk #{__dir__}/test_tail_recursion.rb:18
+│ │ └─walking 3
+│ ├─TestTailRecursion::SomeRecursion#swim #{__dir__}/test_tail_recursion.rb:22
+│ │ └─swim
+│ └─TestTailRecursion::SomeRecursion#jump #{__dir__}/test_tail_recursion.rb:26
+│   └─jump
+├─TestTailRecursion::SomeRecursion#run #{__dir__}/test_tail_recursion.rb:7
+│ ├─TestTailRecursion::SomeRecursion#walk #{__dir__}/test_tail_recursion.rb:18
+│ │ └─walking 2
+│ ├─TestTailRecursion::SomeRecursion#swim #{__dir__}/test_tail_recursion.rb:22
+│ │ └─swim
+│ └─TestTailRecursion::SomeRecursion#jump #{__dir__}/test_tail_recursion.rb:26
+│   └─jump
+├─TestTailRecursion::SomeRecursion#run #{__dir__}/test_tail_recursion.rb:7
+│ ├─TestTailRecursion::SomeRecursion#walk #{__dir__}/test_tail_recursion.rb:18
+│ │ └─walking 1
+│ ├─TestTailRecursion::SomeRecursion#swim #{__dir__}/test_tail_recursion.rb:22
+│ │ └─swim
+│ └─TestTailRecursion::SomeRecursion#jump #{__dir__}/test_tail_recursion.rb:26
+│   └─jump
+└─TestTailRecursion::SomeRecursion#run #{__dir__}/test_tail_recursion.rb:7
+  ├─TestTailRecursion::SomeRecursion#walk #{__dir__}/test_tail_recursion.rb:18
+  │ └─walking 0
+  ├─TestTailRecursion::SomeRecursion#swim #{__dir__}/test_tail_recursion.rb:22
+  │ └─swim
+  └─divided by 0
+EOS
+
   def test_tail_recursion
     some_recursion = SomeRecursion.new
-    Tracia.start(non_tail_recursion: true) do
-      some_recursion.fly
-      some_recursion.run
-    end
-  rescue ZeroDivisionError
-  end
+    sio = StringIO.new
 
+    begin
+      Tracia.start(non_tail_recursion: true, logger: Tracia::DefaultLogger.new(out: sio)) do
+        some_recursion.fly
+        some_recursion.run
+      end
+    rescue ZeroDivisionError
+    end
+
+    sio.rewind
+    assert_equal EXPECTED, sio.read
+  end
 end
