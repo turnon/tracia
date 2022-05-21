70ec7a1131ff9527c3a39be14af284e1904b07b8
ken <block24block@gmail.com>
2022-05-21 12:19:05 +0800

    update tree_graph

--- lib/tracia/default_logger.rb
+++ lib/tracia/default_logger.rb
@@ -1,19 +1,5 @@
 class Tracia
   class DefaultLogger
-    NO_CHILD = []
-
-    class << self
-      def tree_graph_everything!
-        Object.define_method(:label_for_tree_graph) do
-          to_s
-        end
-
-        Object.define_method(:children_for_tree_graph) do
-          NO_CHILD
-        end
-      end
-    end
-
     def initialize(out: STDOUT)
       @out = out
     end
